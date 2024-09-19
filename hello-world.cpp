aed5f10e652838a79edba1698d4cd901b104046d
TejasviYasa <tejasviyasa@gmail.com>
2024-09-18 19:27:37 -0500

    This is the messiest I could make it.

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
     
     // my comment
-    std::cout << "Hello UTD" << std::endl;
-
+    //std::cout << "Hello UTD" << std::endl;
+    int a[11] = {72, 101, 108, 108, 111, 32, 85, 84, 68, 33, 0};
+    string s;
+    for (int i = 0; i < 11; i++) {
+        char l = a[i];
+        s += l;
+        cout << s.substr(i, i+1);
+    }
     return 0;
 }
